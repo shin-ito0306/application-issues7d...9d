9c3028775080716816c7da1376edb3a641c9c930
shin-ito0306 <shin.20000306@icloud.com>
2021-06-11 02:31:07 +0000

    [add]form_category

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,7 +51,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :star)
+    params.require(:book).permit(:title, :body, :star, :category)
   end
 
   def ensure_correct_user
